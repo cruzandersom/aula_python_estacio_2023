9035bade8bd923ac665675c48fb614b9256dda49
cruzandersom <cruz.andersom@gmail.com>
2023-05-19 21:35:31 -0300

    primeira_parte_aula_19052022

--- conta.py
+++ conta.py
@@ -1,7 +1,7 @@
 class Conta:
 
     def __init__(self, titular: str, numero: int,
-                saldo: float, limite: float):
+                 saldo: float, limite: float):
         self.__titular: str = titular
         self.__numero: int = numero
         self.__saldo: float = saldo
@@ -11,10 +11,14 @@ class Conta:
         self.__saldo += valor
 
     def saca(self, valor: float) -> None:
-        if self.__saldo >= valor:
+        if self.__pode_sacar(valor):
             self.__saldo -= valor
+            print("Saque realizado com sucesso")
         else:
             print("Não foi possível realizar o saque\nSaldo insuficiente")
+
+    def __pode_sacar(self, valor: float) -> bool:
+        return self.__saldo >= valor
 
     def transfere(self, valor: float, destino):
         self.saca(valor)
@@ -23,9 +27,10 @@ class Conta:
     def extrato(self) -> None:
         print("Saldo de {} do titular {}".format(self.__saldo, self.titular))
 
-
-    def get_saldo(self) -> float:
+    @property
+    def saldo(self) -> float:
         return self.__saldo
 
-    def set_saldo(self, saldo: float) -> None:
+    @saldo.setter
+    def saldo(self, saldo: float) -> None:
         self.__saldo = saldo
--- testa_conta.py
+++ testa_conta.py
@@ -6,8 +6,11 @@ print(conta)
 conta = Conta('João', 123, 55.5, 1000.0)
 print(conta)
 
-print(f"Esse é o saldo da conta {conta.get_saldo()}")
+print(f"Esse é o saldo da conta {conta.saldo}")
 
 conta.deposita(100.0)
 
-print(f"Esse é o saldo da conta{conta.get_saldo()}")
+print(f"Esse é o saldo da conta{conta.saldo}")
+
+conta.saca(100.00)
+print(f"Esse é o saldo da conta{conta.saldo}")
